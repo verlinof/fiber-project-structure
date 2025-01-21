e8130bc242e53aac8560fa6ab6a1c3f228505330
verlinof <ajikoko.zx@gmail.com>
2025-01-21 11:58:29 +0700

    Feat: Fiber Configuration

--- configs/app_config/type.go
+++ configs/app_config/type.go
@@ -1,7 +1,6 @@
 package app_config
 
 type AppConfig struct {
-	GinMode      string `env:"GIN_MODE"`
 	AppPort      string `env:"APP_PORT"`
 	AppUrl       string `env:"APP_URL"`
 	JwtSecretKey string `env:"JWT_SECRET_KEY"`
