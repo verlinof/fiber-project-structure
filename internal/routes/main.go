136168cf661f5ea59d3c744e33428d4f73450bcc
verlinof <marimo.zx@gmail.com>
2026-09-15 00:18:11 +0700

    Fix<things>

--- internal/routes/main.go
+++ internal/routes/main.go
@@ -1,15 +1,12 @@
 package routes
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"github.com/verlinof/fiber-project-structure/db"
 	auth_http "github.com/verlinof/fiber-project-structure/internal/modules/auth/http"
 	auth_route "github.com/verlinof/fiber-project-structure/internal/modules/auth/http/route"
 	auth_service "github.com/verlinof/fiber-project-structure/internal/modules/auth/service"
-	example_http "github.com/verlinof/fiber-project-structure/internal/modules/example/http"
-	example_route "github.com/verlinof/fiber-project-structure/internal/modules/example/http/route"
-	example_service "github.com/verlinof/fiber-project-structure/internal/modules/example/service"
 	pkg_validation "github.com/verlinof/fiber-project-structure/pkg/validation"
-	"github.com/gofiber/fiber/v2"
 )
 
 func InitRoute(app *fiber.App) {
@@ -28,13 +25,8 @@ func InitRoute(app *fiber.App) {
 
 	// Services
 	authService := auth_service.NewService(db.GetDB())
-	exampleService := example_service.NewService(db.GetDB())
 
 	// Auth Module Routes
 	authHandler := auth_http.NewHandler(authService, validator)
 	auth_route.InitRoute(v1, authHandler)
-
-	// Example Module Routes (Contoh pendaftaran modul baru)
-	exampleHandler := example_http.NewHandler(exampleService, validator)
-	example_route.InitRoute(v1, exampleHandler)
 }
